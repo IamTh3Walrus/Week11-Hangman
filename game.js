5c6d9aa5cd832eb9b2e2cd1d58a9dade65278232
Stephen Doe <redcard1016@yahoo.com>
2016-07-27 16:23:30 -0400

    AAAAAAAAGGGGGGGGGHHHHHHH

---
+++ game.js
@@ -0,0 +1,8 @@
+exports.game = {
+	wordBank : ["Love Me Do", "From Me to You, She Loves You", "I Want to Hold Your Hand",
+"Can't Buy Me Love", "A Hard Day's Night", "I Feel Fine", "Eight Days a Week", "Ticket to Ride",
+"Help", "Yesterday", "Day Tripper", "We Can Work It Out", "Paperback Writer", "Yellow Submarine",
+"Eleanor Rigby", "Penny Lane", "All You Need Is Love",
+"Hello Goodbye", "Lady Madonna", "Hey Jude", "Get Back", "The Ballad of John and Yoko",
+"Something", "Come Together", "Let It Be", "The Long and Winding Road"]
+};
